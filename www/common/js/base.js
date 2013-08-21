180b16f0a2bc5d71f84e3fa2eef3ae10c49f5319
zhangsh <simon.p.7@hotmail.com>
2013-08-21 10:57:54 +0800

    test json

--- www/common/js/base.js
+++ www/common/js/base.js
@@ -1,4 +1,5 @@
 window.win = window, win.doc = win.document, win.sess = win.sessionStorage;
+win.host = "http://10.0.2.2:12411/", win.volHost = host + "VolunteerWebService.asmx/";
 doc.addEventListener("deviceready", function(e) {
 	navigator.notification && (win.nav = navigator, win.alert = nav.notification.alert, win.confirm = nav.notification.confirm);
 }, false);
