d2b9a5d36cf066b22ae9352934261dcfe93d1164
zhangsh <simon.p.7@hotmail.com>
2013-08-23 14:15:10 +0800

    加入加入活动

--- www/js/activity.js
+++ www/js/activity.js
@@ -1,9 +1,9 @@
 $(function() {
-	var id = getItem("activity_id");
-	id = id.substring(2, id.length);
-	$.post(volService + "Activity", { pid: id, had: getItem("activity_had") }, function(data) {
+	var aid = getItem("activity_id");
+	aid = id.substring(2, aid.length);
+	$.post(volService + "Activity", { id: getUser(), pid: aid, had: getItem("activity_had") }, function(data) {
 		var d = XML2JSON(data);
-		d[22] === "0" && $("join").remove();
+		d[22] === "0" && $("article").remove("#join");
 		$("#team").val(d[0]);
 		$("#squad").val(d[1]);
 		$("#type").val(d[2]);
@@ -26,4 +26,14 @@ $(function() {
 		$("#detail").val(d[20]);
 		d[21] && $("#attachment").attr("href", filePath + d[21]).html("点击下载");
 	}, "xml");
+	
+	$("#join").click(function(e) {
+        $.post(volService + "Join", { id: getUser(), pid: aid }, function(data) {
+			var d = XML2JSON(data);
+			if (d === 0) alert("服务器错误！", null, "对不起", "确 定");
+			else {
+				alert("加入成功！", null, "恭喜你", "确 定");
+			}
+		}, "xml");
+    });
 })
