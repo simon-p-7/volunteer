0f636482dc467960c1de69cfb4e9b06911066649
zhangsh <simon.p.7@hotmail.com>
2013-08-26 11:18:20 +0800

    fix

--- www/js/participate.js
+++ www/js/participate.js
@@ -3,7 +3,7 @@ $(function() {
 	$.post(volService + "Participate", { id: getUser() }, function(data) {
 		var d = XML2JSON(data);
 		for (var i = 0, arr; arr = d[i++];)
-			ul.append("<li id='a_" + arr[0] + "'>" + arr[1] + "(" + arr[2] + ") [" + arr[3] + "]</li>");
+			ul.append("<li id='a_" + arr[0] + "'>" + arr[1] + "(时长：" + arr[2] + ") [" + arr[3] + "]</li>");
 	}, "xml");
 	
 	ul.click(function(e) {
