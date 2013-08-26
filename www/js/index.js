5f01a495624234f73cfe75daef1a057e16ef8327
zhangsh <simon.p.7@hotmail.com>
2013-08-26 16:00:23 +0800

    加入 Barcode Scanner

--- www/js/index.js
+++ www/js/index.js
@@ -11,4 +11,17 @@ $(function() {
 		setItem("user_id", "7");
 		redirect("info.html");
 	}
+	
+	$("#scan").click(function(e) {
+        try {
+            var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+            scanner.scan(function(res) {
+				res.cancelled || alert(res.format + ": " + res.text);
+            }, function(error) { 
+                alert(error); 
+            });
+        } catch (ex) {
+            alert(ex.message);
+        }
+    });
 })
