0eb7e448cb4e095a5d924d65cfa7d752abf01e88
zhangsh <simon.p.7@hotmail.com>
2013-09-02 16:37:47 +0800

    fix

--- www/js/tem/activities.js
+++ www/js/tem/activities.js
@@ -3,14 +3,14 @@ $(function() {
 	$.get(temService + "Activities", { id: getUser() }, function(data) {
 		var d = XML2JSON(data);
 		for (var i = 0, arr; arr = d[i++];) {
-			var btn = "<button type='button'>扫描</button>";
+			var btn = "<button type='button' id='ba_" + arr[0] + "'>扫描</button>";
 			ul.append("<li id='a_" + arr[0] + "'>" + arr[1] + "(" + arr[2] + ") [" + arr[3] + "]" + btn + "</li>");
 		}
 	}, "xml");
 	
 	ul.click(function(e) {
 		if (e.target.type === "button") {
-			alert(e.target.parent.id);
+			alert(e.target.id);
 		} else {
 			setItem("activity_id", e.target.id);
 			redirect("activity.html");
