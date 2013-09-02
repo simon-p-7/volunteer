e00ece9c4a63841f50f1209a0ce4fb17a7aafd6a
zhangsh <simon.p.7@hotmail.com>
2013-09-02 09:50:22 +0800

    加入 我管理的活动列表

---
+++ www/js/tem/activities.js
@@ -0,0 +1,13 @@
+$(function() {
+	var ul = $("article ul").html("");
+	$.get(temService + "Activities", { id: getUser() }, function(data) {
+		var d = XML2JSON(data);
+		for (var i = 0, arr; arr = d[i++];)
+			ul.append("<li id='a_" + arr[0] + "'>" + arr[1] + "(" + arr[2] + ") [" + arr[3] + "]</li>");
+	}, "xml");
+	
+	ul.click(function(e) {
+        setItem("activity_id", e.target.id);
+		redirect("activity.html");
+    });
+})
