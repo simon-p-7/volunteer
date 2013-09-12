9c01724773970dd0279658dbe91438d4d58049f1
zhangsh <simon.p.7@hotmail.com>
2013-09-12 14:59:23 +0800

    fix login

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,16 @@
 $(function() {
-	$("#txt_un").focus().keydown(function(e) {
+	$("#txt_un").keydown(function(e) {
         e.which === 13 && $("#txt_pw").focus();
     });
 	$("#txt_pw").keydown(function(e) {
        e.which === 13 && login(e);
     });
 	$("#btn_li").click(login);
+	
+	$("#vol,#tem").change(function(e) {
+        $(e.target).next().removeClass("unchecked").addClass("checked");
+		$("#" + (e.target.id == "vol" ? "tem" : "vol")).next().removeClass("checked").addClass("unchecked");
+    });
 	
 	function login(e) {
 		setItem("user_type", $("#tem").prop("checked") ? "tem" : "vol");
