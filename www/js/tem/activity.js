26209156b1047de02e0014835704d75d6ef5c77e
zhangsh <simon.p.7@hotmail.com>
2013-09-12 09:23:59 +0800

    fix

--- www/js/tem/activity.js
+++ www/js/tem/activity.js
@@ -1,19 +1,19 @@
 $(function() {
 	var aid = getId(getItem("activity_id"));
-	var ul = $("article ul").html("");
+	var ul = $("section ul").html("");
 	$.get(temService + "Activity", { pid: aid }, function(data) {
 		var d = XML2JSON(data);
 		$("#title").html(d[0]);
 		$("#location").val(d[1]);
 		$("#date").val(d[2]);
 		for (var i = 0, arr; arr = d[3][i++];)
-			ul.append("<li id='u_" + arr[0] + "'><div></div>" + arr[1] + "(" + arr[2] + ") [" + arr[3] + "]</li>");
+			ul.append("<li id='u_" + arr[0] + "'><div></div>" + arr[1] + " (" + arr[2] + ")<span>" + arr[3] + "</span></li>");
 	}, "xml");
 	
 	ul.click(function(e) {
 		var u = $(e.target);
 		vibrate(210);
-		confirm("确定要删除 " + u.html() + " 的服务时长吗？", function(btn) {
+		confirm("确定要删除 " + u.text() + " 的服务时长吗？", function(btn) {
 			btn === 1 && $.get(temService + "Delete", { pid: aid, id: getId(e.target.id) }, function(data) {
 				XML2JSON(data) ? u.remove() : sorry("删除时出错！");
 			}, "xml");
