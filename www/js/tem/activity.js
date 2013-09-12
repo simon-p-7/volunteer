500fc02bae101b1359473448d603acda0abf0165
zhangsh <simon.p.7@hotmail.com>
2013-09-12 10:41:38 +0800

    fix

--- www/js/tem/activity.js
+++ www/js/tem/activity.js
@@ -11,9 +11,9 @@ $(function() {
 	}, "xml");
 	
 	ul.click(function(e) {
-		var u = $(e.target);
+		var u = $(e.target.parent);
 		vibrate(210);
-		confirm("确定要删除 " + u.text() + " 的服务时长吗？", function(btn) {
+		confirm("确定要删除 " + u.find("span").eq(1).text() + " 的服务时长吗？", function(btn) {
 			btn === 1 && $.get(temService + "Delete", { pid: aid, id: getId(e.target.id) }, function(data) {
 				XML2JSON(data) ? u.remove() : sorry("删除时出错！");
 			}, "xml");
