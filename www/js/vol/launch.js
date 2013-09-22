ab207397899a3da588e1f0676375fe7becc40813
zhangsh <simon.p.7@hotmail.com>
2013-09-22 11:28:37 +0800

    fix

--- www/js/vol/launch.js
+++ www/js/vol/launch.js
@@ -3,11 +3,16 @@ $(function() {
 	$.get(volService + "Launch", null, function(data) {
 		var d = XML2JSON(data);
 		for (var i = 0, arr; arr = d[i++];)
-			ul.append("<li id='a_" + arr[0] + "'>" + arr[1] + " [" + arr[2] + "]</li>");
+			ul.append("<li id='a_" + arr[0] + "'><span>" + arr[1] + "<br><span>" + arr[2] + "</span></span></li>");
 	}, "xml");
 	
 	ul.click(function(e) {
-        setItem("activity_id", e.target.id);
+        var tag = e.target;
+		if (tag.nodeName.toUpperCase() !== "LI") {
+			while (tag.parentNode.nodeName.toUpperCase() !== "LI") tag = tag.parentNode;
+			tag = tag.parentNode;
+		}
+		setItem("activity_id", tag.id);
 		setItem("activity_had", false);
 		redirect("activity.html");
     });
