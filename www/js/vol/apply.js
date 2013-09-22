8f49c87aad3493d560eaa81eb951aef2e9a1b30d
zhangsh <simon.p.7@hotmail.com>
2013-09-22 11:48:20 +0800

    fix

--- www/js/vol/apply.js
+++ www/js/vol/apply.js
@@ -3,7 +3,7 @@ $(function() {
 	$.get(volService + "Apply", { id: getUser() }, function(data) {
 		var d = XML2JSON(data);
 		for (var i = 0, arr; arr = d[i++];)
-			ul.append("<li id='a_" + arr[0] + "'><span>" + arr[1] + "<br><span>" + arr[2] + "</span><span>时长：" + arr[3] + "小时</span></span></li>");
+			ul.append("<li id='a_" + arr[0] + "'><span>" + arr[1] + "<br><span>" + arr[3] + "</span><span>" + arr[2] + "</span></span></li>");
 	}, "xml");
 	
 	ul.click(function(e) {
