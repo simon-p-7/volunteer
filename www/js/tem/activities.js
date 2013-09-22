43a45eb53a563e182105381b0bc00f96ccaa841c
zhangsh <simon.p.7@hotmail.com>
2013-09-22 15:12:38 +0800

    fix

--- www/js/tem/activities.js
+++ www/js/tem/activities.js
@@ -3,7 +3,7 @@ $(function() {
 	$.get(temService + "Activities", { id: getUser() }, function(data) {
 		var d = XML2JSON(data);
 		for (var i = 0, arr; arr = d[i++];) {
-			var btn = "<button type='button' id='a_" + arr[0] + "'>&nbsp;<img src='../img/qr.png'>&nbsp;</button>";
+			var btn = "<button type='button' id='a_" + arr[0] + "'><img src='../img/qr.png'></button>";
 			ul.append("<li id='a_" + arr[0] + "'><span>" + arr[1] + "<br><span>" + arr[3] + "</span><span>" + arr[2] + "</span></span>" + btn + "</li>");
 		}
 	}, "xml");
