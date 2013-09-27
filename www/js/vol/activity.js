ac866b65df405017704b5dc9b8fc80479101e130
zhangsh <simon.p.7@hotmail.com>
2013-09-27 15:24:40 +0800

    bug

--- www/js/vol/activity.js
+++ www/js/vol/activity.js
@@ -3,29 +3,26 @@ $(function() {
 	var aid = getId(getItem("activity_id"));
 	ajaxGet(volService + "Activity", { id: getUser(), pid: aid, had: getItem("activity_had") }, function(data) {
 		var d = XML2JSON(data);
-		d[22] === "0" && $("#join").remove();
+		d[19] === "0" && $("#join").remove();
 		$("#team").val(d[0]);
 		$("#squad").val(d[1]);
 		$("#type").val(d[2]);
 		$("#title").html(d[3]);
-		$("#unit").val(d[4]);
-		$("#array").val(d[5]);
-		$("#contact").val(d[6]);
-		$("#phone").val(d[7]);
-		$("#number").val(d[8]);
-		$("#long").val(d[9]);
-		$("#content").val(d[10]);
-		$("#target").val(d[11]);
-		$("#claim").val(d[12]);
-		$("#recruit").val(d[13]);
-		$("#location").val(d[14]);
-		$("#status").val(d[15]);
-		$("#date").val(d[16]);
-		$("#time1").val(d[17]);
-		$("#time2").val(d[18]);
-		$("#participate").val(d[19]);
-		$("#detail").val(d[20]);
-		d[21] && $("#attachment").attr("href", filePath + d[21]).html("点击下载");
+		$("#contact").val(d[4]);
+		$("#phone").val(d[5]);
+		$("#number").val(d[6]);
+		$("#long").val(d[7]);
+		$("#content").val(d[8]);
+		$("#claim").val(d[9]);
+		$("#recruit").val(d[10]);
+		$("#location").val(d[11]);
+		$("#status").val(d[12]);
+		$("#date").val(d[13]);
+		$("#time1").val(d[14]);
+		$("#time2").val(d[15]);
+		$("#participate").val(d[16]);
+		$("#detail").val(d[17]);
+		d[18] && $("#attachment").attr("href", filePath + d[18]).html("点击下载");
 	});
 	
 	$("#join").click(function(e) {
--- www/js/vol/info.js
+++ www/js/vol/info.js
@@ -4,22 +4,18 @@ $(function() {
 		var d = XML2JSON(data);
 		$("#uname").val(d[0]);
 		$("#name").val(d[1]);
-		$("#id").val(d[2]);
-		$("#asso").val(d[3]);
-		$("#team").val(d[4]);
-		$("#squad").val(d[5]);
-		$("#spec").val(d[6]);
-		$("#unit").val(d[7]);
-		$("#phone").val(d[8]);
-		$("#type").val(d[9]);
-		$("#email").val(d[10]);
-		$("#free").val(d[11]);
-		$("#edu").val(d[12]);
-		$("#gender").val(d[13]);
-		$("#exp").val(d[14]);
-		$("#head").attr("src", skinPath + d[15]);
-		$("#hour").val(d[16]);
-		$("#star").val(d[17]);
+		$("#asso").val(d[2]);
+		$("#team").val(d[3]);
+		$("#squad").val(d[4]);
+		$("#spec").val(d[5]);
+		$("#unit").val(d[6]);
+		$("#type").val(d[7]);
+		$("#email").val(d[8]);
+		$("#free").val(d[9]);
+		$("#exp").val(d[10]);
+		$("#head").attr("src", skinPath + d[11]);
+		$("#hour").val(d[12]);
+		$("#star").val(d[13]);
 	});
 	
 	$("button,nav ul li").click(function(e) {
