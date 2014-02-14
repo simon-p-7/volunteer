c743edee929e33468316d7ff765e990d6a72b3c9
zhangsh <simon.p.7@hotmail.com>
2014-02-14 15:01:16 +0800

    fix

--- www/common/js/base.js
+++ www/common/js/base.js
@@ -1,6 +1,6 @@
 window.win = window, win.doc = win.document, win.sess = win.sessionStorage, win.stag = win.localStorage;
 win.host = "http://192.168.1.99:", win.serviceHost = host + "2544/", win.fileHost = host + "15041/";
-win.volService = serviceHost + "VolunteerWebService.asmx/", win.temService = serviceHost + "TeamWebService.asmx/", win.nwsService = serviceHost + "NewsWebService.asmx", win.hlpService = serviceHost + "HelpWebService.asmx";
+win.volService = serviceHost + "VolunteerWebService.asmx/", win.temService = serviceHost + "TeamWebService.asmx/", win.nwsService = serviceHost + "NewsWebService.asmx/", win.hlpService = serviceHost + "HelpWebService.asmx/";
 win.skinPath = fileHost + "Skin/Images/", win.filePath = fileHost + "Upfiles/", win.teamPath = fileHost + "Module/FilesUp/UserIcon/";
 win.needAskExit, win.needAskLogout;
 doc.addEventListener("deviceready", function(e) {
