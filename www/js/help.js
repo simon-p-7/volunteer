c920704cc32b073e2b71f01374fb9e0e99c45f55
zhangsh <simon.p.7@hotmail.com>
2014-02-14 16:30:57 +0800

    test help

--- www/js/help.js
+++ www/js/help.js
@@ -1,46 +1,21 @@
 needAskLogout = false;
 $(function() {
-	$("section ul").keydown(function(e) {
-		alert(e.target);
-		// if (e.which === 13) {
-		// 	switch (e.target.id) {
-		// 		case "txt_title":
-		// 			$("#txt_name").focus();
-		// 			break;
-		// 		case "txt_name":
-		// 			$("#txt_age").focus();
-		// 			break;
-		// 		case "txt_age":
-		// 			$("#txt_phone").focus();
-		// 			break;
-		// 		case "txt_phone":
-		// 			$("#txt_address").focus();
-		// 			break;
-		// 		case "txt_address":
-		// 			$("#txt_content").focus();
-		// 			break;
-		// 		case "txt_content":
-		// 			$("#commit").focus();
-		// 			break;
-		// 	}
-		// }
-    });
-
 	$("#commit").click(function(e) {
-		alert("here");
-	// 	var titlev = $("#txt_title").val();
-	// 	var namev = $("#txt_name").val();
-	// 	var agev = $("#txt_age").val();
-	// 	var phonev = $("#txt_phone").val();
-	// 	var addressv = $("#txt_address").val();
-	// 	var contentv = $("#txt_content").val();
+		var titlev = $("#txt_title").val();
+		var namev = $("#txt_name").val();
+		var agev = $("#txt_age").val();
+		var phonev = $("#txt_phone").val();
+		var addressv = $("#txt_address").val();
+		var contentv = $("#txt_content").val();
 
-	// 	if (titlev === "" || namev === "" || agev === "" || phonev === "" || addressv === "" || contentv === "")
-	// 		sorry("请您完善求助信息后再提交！");
-	// 	else
-	// 		ajaxGet(hlpService + "Help", { title: titlev, name: namev, age: agev, phone: phonev, address: addressv, content: contentv }, function(data) {
-	// 			var d = XML2JSON(data);
-	// 			d === 0 ? sorry("提交失败！") ： good("提交成功！");
-	// 		});
+		alert("here");
+
+		if (titlev === "" || namev === "" || agev === "" || phonev === "" || addressv === "" || contentv === "")
+			sorry("请您完善求助信息后再提交！");
+		else
+			ajaxGet(hlpService + "Help", { title: titlev, name: namev, age: agev, phone: phonev, address: addressv, content: contentv }, function(data) {
+				var d = XML2JSON(data);
+				d === 0 ? sorry("提交失败！") ： good("提交成功！");
+			});
 	});
 })
