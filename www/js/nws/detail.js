6c077fd63a5b8580575da0c0e7236bebd2641ecf
zhangsh <simon.p.7@hotmail.com>
2014-02-15 12:00:27 +0800

    fix news detail

--- www/js/nws/detail.js
+++ www/js/nws/detail.js
@@ -2,7 +2,7 @@ needAskLogout = false;
 $(function() {
     ajaxGet(nwsService + "Detail", { id: getId(getItem("news_id")) }, function(data) {
         var d = XML2JSON(data);
-        $("#title").val(d[0]);
+        $("#title").html(d[0]);
         $("#source").val(d[1]);
         $("#time").val(d[2]);
         $("section").html(d[3]);
