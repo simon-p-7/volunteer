826ed1a363470fca48b11574d78d55e579be95e9
zhangsh <simon.p.7@hotmail.com>
2014-02-17 17:40:32 +0800

    fix

--- www/js/nws/detail.js
+++ www/js/nws/detail.js
@@ -5,12 +5,6 @@ $(function() {
         $("#title").html(d[0]);
         $("#source").val(d[1]);
         $("#time").val(d[2]);
-        $("section").html(d[3]);
-
-        $("section img[src^='../']").each(function(i) {
-            alert(this.src);
-            alert(fileHost + this.src.replace("../", ""));
-        	this.src = fileHost + this.src.replace("../", "");
-        });
+        $("section").html(d[3].replace("src=\"../", "src=\"" + fileHost));
     });
 })
