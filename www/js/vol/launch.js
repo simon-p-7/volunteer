33a08c943b53805d29e9caac2a7e20870fc23afc
Simon <simon.p.7@hotmail.com>
2014-03-01 17:18:39 +0800

    add nav

--- www/js/vol/launch.js
+++ www/js/vol/launch.js
@@ -1,20 +1,37 @@
 needAskLogout = false;
-$(function() {
-	var ul = $("article ul").html("");
-	ajaxGet(volService + "Launch", null, function(data) {
-		var d = XML2JSON(data);
-		for (var i = 0, arr; arr = d[i++];)
-			ul.append("<li id='a_" + arr[0] + "'><span>" + arr[1] + "<br><span>" + arr[2] + "</span><span>&nbsp;</span></span></li>");
-	});
-	
-	ul.click(function(e) {
+$(function () {
+    getUser() === -1 ? $("article").css("padding-bottom", "11em") : $("nav").remove();
+
+    var ul = $("article ul").html("");
+    ajaxGet(volService + "Launch", null, function (data) {
+        var d = XML2JSON(data);
+        for (var i = 0, arr; arr = d[i++]; )
+            ul.append("<li id='a_" + arr[0] + "'><span>" + arr[1] + "<br><span>" + arr[2] + "</span><span>&nbsp;</span></span></li>");
+    });
+
+    ul.click(function (e) {
         var tag = e.target;
-		if (tag.nodeName.toUpperCase() !== "LI") {
-			while (tag.parentNode.nodeName.toUpperCase() !== "LI") tag = tag.parentNode;
-			tag = tag.parentNode;
-		}
-		setItem("activity_id", tag.id);
-		setItem("activity_had", false);
-		redirect("activity.html");
+        if (tag.nodeName.toUpperCase() !== "LI") {
+            while (tag.parentNode.nodeName.toUpperCase() !== "LI") tag = tag.parentNode;
+            tag = tag.parentNode;
+        }
+        setItem("activity_id", tag.id);
+        setItem("activity_had", false);
+        redirect("activity.html");
+    });
+
+    $("nav ul").click(function (e) {
+        var tag = e.target;
+        if (tag.nodeName.toUpperCase() !== "LI") {
+            while (tag.parentNode.nodeName.toUpperCase() !== "LI") tag = tag.parentNode;
+            tag = tag.parentNode;
+        }
+        switch (tag.id) {
+            case "home": redirect("../index.html"); break;
+            case "news": redirect("list.html"); break;
+            case "activity": redirect("../vol/launch-activity.html"); break;
+            case "help": redirect("../help.html"); break;
+            case "login": redirect("../login.html"); break;
+        }
     });
 })
