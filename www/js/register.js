7916711888538656e73d7f6e043d33b45983cfe3
simon <simon.p.7@hotmail.com>
2014-03-07 09:51:46 +0800

    fix

--- www/js/register.js
+++ www/js/register.js
@@ -16,4 +16,53 @@ $(function () {
             d === 0 ? sorry("提交信息失败！") : (good("提交信息成功！"), goBack());
         });
     });
+
+    function isIdCardNo(num) {
+        var factorArr = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2, 1);
+        var parityBit = new Array("1", "0", "X", "9", "8", "7", "6", "5", "4", "3", "2");
+        var varArray = new Array();
+        var intValue;
+        var lngProduct = 0;
+        var intCheckDigit;
+        var intStrLen = num.length;
+        var idNumber = num;
+        // initialize
+        if ((intStrLen != 15) && (intStrLen != 18)) {
+            return false;
+        }
+        // check and set value
+        for (i = 0; i < intStrLen; i++) {
+            varArray[i] = idNumber.charAt(i);
+            if ((varArray[i] < '0' || varArray[i] > '9') && (i != 17)) {
+                return false;
+            } else if (i < 17) {
+                varArray[i] = varArray[i] * factorArr[i];
+            }
+        }
+        if (intStrLen == 18) {
+            //check date
+            var date8 = idNumber.substring(6, 14);
+            if (isDate8(date8) == false) {
+                return false;
+            }
+            // calculate the sum of the products
+            for (i = 0; i < 17; i++) {
+                lngProduct = lngProduct + varArray[i];
+            }
+            // calculate the check digit
+            intCheckDigit = parityBit[lngProduct % 11];
+            // check last digit
+            if (varArray[17] != intCheckDigit) {
+                return false;
+            }
+        }
+        else {        //length is 15
+            //check date
+            var date6 = idNumber.substring(6, 12);
+            if (isDate6(date6) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
 })
--- www/js/vol/info.js
+++ www/js/vol/info.js
@@ -1,23 +1,24 @@
 needAskLogout = true;
-$(function() {
-	ajaxGet(volService + "UserInfo", { id: getUser() }, function(data) {
-		var d = XML2JSON(data);
-		$("#uname").val(d[0]);
-		$("#name").val(d[1]);
-		$("#asso").val(d[2]);
-		$("#team").val(d[3]);
-		$("#squad").val(d[4]);
-		$("#spec").val(d[5]);
-		$("#unit").val(d[6]);
-		$("#type").val(d[7]);
-		$("#email").val(d[8]);
-		$("#head").attr("src", skinPath + d[9]);
-		$("#hour").val(d[10]);
-		$("#star").val(d[11]);
-	});
-	
-	$("button,nav ul li").click(function(e) {
-		var tag = e.target;
+$(function () {
+    ajaxGet(volService + "UserInfo", { id: getUser() }, function (data) {
+        var d = XML2JSON(data);
+        $("#uname").val(d[0]);
+        $("#name").val(d[1]);
+        $("#asso").val(d[2]);
+        $("#team").val(d[3]);
+        $("#squad").val(d[4]);
+        $("#head").attr("src", skinPath + d[5]);
+        $("#hour").val(d[6]);
+        $("#star").val(d[7]);
+        if (d[8] === "") {
+            $("qr").remove();
+        } else {
+            $("#qr").attr("src", d[8]);
+        }
+    });
+
+    $("button,nav ul li").click(function (e) {
+        var tag = e.target;
         redirect((tag.nodeName.toUpperCase() === "IMG" ? tag.parentNode.id : tag.id) + "-activity.html");
     });
 });
