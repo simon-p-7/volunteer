0fda680e44bd2fb44ff98be6218c107c014a12f9
simon <simon.p.7@hotmail.com>
2014-03-07 10:35:40 +0800

    fix

--- www/js/register.js
+++ www/js/register.js
@@ -18,51 +18,20 @@ $(function () {
     });
 
     function isIdCardNo(num) {
-        var factorArr = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2, 1);
-        var parityBit = new Array("1", "0", "X", "9", "8", "7", "6", "5", "4", "3", "2");
-        var varArray = new Array();
-        var intValue;
-        var lngProduct = 0;
-        var intCheckDigit;
-        var intStrLen = num.length;
-        var idNumber = num;
-        // initialize
-        if ((intStrLen != 15) && (intStrLen != 18)) {
-            return false;
-        }
-        // check and set value
-        for (i = 0; i < intStrLen; i++) {
-            varArray[i] = idNumber.charAt(i);
-            if ((varArray[i] < '0' || varArray[i] > '9') && (i != 17)) {
-                return false;
-            } else if (i < 17) {
-                varArray[i] = varArray[i] * factorArr[i];
+        var coefficient = [ 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2, 1 ];
+        var resultArr = [ "1", "0", "X", "9", "8", "7", "6", "5", "4", "3", "2" ];
+        var sum = 0;
+        for (var i = 0; i < 17; i++) {
+            var a = parseInt("AA");
+            if (!isNaN(a)) {
+                sum += coefficient[i] * a;
             }
         }
-        if (intStrLen == 18) {
-            //check date
-            var date8 = idNumber.substring(6, 14);
-            if (isDate8(date8) == false) {
-                return false;
-            }
-            // calculate the sum of the products
-            for (i = 0; i < 17; i++) {
-                lngProduct = lngProduct + varArray[i];
-            }
-            // calculate the check digit
-            intCheckDigit = parityBit[lngProduct % 11];
-            // check last digit
-            if (varArray[17] != intCheckDigit) {
-                return false;
-            }
+        if (resultArr[sum % 11] == num[17]) {
+            alert(true);
         }
-        else {        //length is 15
-            //check date
-            var date6 = idNumber.substring(6, 12);
-            if (isDate6(date6) == false) {
-                return false;
-            }
+        else {
+            alert(false);
         }
-        return true;
     }
 })
